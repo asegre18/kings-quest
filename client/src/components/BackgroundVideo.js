71eb27449251759ef827a9a9e699256ef1ac48ff
ReverbDesigns <jordymcbell@gmail.com>
2020-12-16 18:07:03 -0800

    fixed main page button routing issues

--- client/src/components/BackgroundVideo.js
+++ client/src/components/BackgroundVideo.js
@@ -1,90 +1,8 @@
 import classes from '../style/BackgroundVideo.module.css';
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { WrappedSignIn } from './SignIn'
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import { Link } from '@material-ui/core'
 
-const SignInButton = withStyles({
-  root: {
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 22,
-    padding: '14px 26px',
-    border: '3px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#DA2900',
-    borderColor: '#B19E07',
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#502419',
-      borderColor: '#A7940A',
-      boxShadow: 'none',
-    },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#FF3C2F',
-      borderColor: '#FF3C2F',
-    },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(152,156,40,.5)',
-    },
-  },
-})(Button);
-const SignUpButton = withStyles({
-    root: {
-      boxShadow: 'none',
-      textTransform: 'none',
-      fontSize: 22,
-      padding: '14px 26px',
-      border: '3px solid',
-      lineHeight: 1.5,
-      backgroundColor: '#DA2900',
-      borderColor: '#B19E07',
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:hover': {
-        backgroundColor: '#A7940A',
-        borderColor: '#502419',
-        boxShadow: 'none',
-      },
-      '&:active': {
-        boxShadow: 'none',
-        backgroundColor: '#BBAA38',
-        borderColor: '#BBAA38',
-      },
-      '&:focus': {
-        boxShadow: '0 0 0 0.2rem rgba(187,170,56,.5)',
-      },
-    },
-  })(Button);
 
-  const useStyles = makeStyles((theme) => ({
-    margin: {
-      margin: theme.spacing(1),
-    },
-  }));
+
   
 
 const BackgroundVideo = () => {
@@ -101,14 +19,7 @@ const BackgroundVideo = () => {
                     
                     
                         <div>
-                    <SignInButton variant="contained" color="primary" disableRipple className={classes.margin} size="large" linkTo={WrappedSignIn} style={{
-                        marginRight    : '60px',
-                    
-                    }}>Sign In</SignInButton>
-                    <SignUpButton variant="contained" color="primary" disableRipple className={classes.margin} size="large" linkTo={WrappedSignIn} style={{
-                        marginRight    : '60px',
-                    
-                    }}>Sign Up</SignUpButton>
+        
                     </div>
                    
                 </div>
